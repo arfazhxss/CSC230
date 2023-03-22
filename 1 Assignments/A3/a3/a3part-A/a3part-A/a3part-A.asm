b36993a3a4b4779d42a4ac38e0eccfbc41453cbd
Arfaz Hussain <arfazhussain@uvic.ca>
2023-03-22 15:38:13 -0700

    Routine Commit: Wed 22 Mar 2023 15:38:13 PDT @arfazhxss

--- 1 Assignments/A3/a3/a3part-A/a3part-A/a3part-A.asm
+++ 1 Assignments/A3/a3/a3part-A/a3part-A/a3part-A.asm
@@ -125,6 +125,22 @@ reset:
 call lcd_init
 call lcd_clr
 
+clr r23
+ldi r23, 0x01
+sts BUTTON_IS_PRESSED, r23
+ldi r16, 0x87  ;0x87 = 0b10000111
+sts ADCSRA_BTN, r16
+
+ldi r16, 0x00
+sts ADCSRB_BTN, r16
+ldi r16, 0x40  ;0x40 = 0b01000000
+sts ADMUX_BTN, r16
+
+ldi r16, 0;
+mov BOUNDARY_L, r16
+ldi r16, high(BUTTON_SELECT_ADC)
+mov BOUNDARY_H, r16
+
 ; .def temp=r26
 ; .def templow=r01
 ; .def temphigh=r02
@@ -241,31 +257,16 @@ start:
 	;
 	;
 	;
-	clr r23
-	ldi r23, 0x01
-	sts BUTTON_IS_PRESSED, r23
-	ldi r16, 0x87  ;0x87 = 0b10000111
-	sts ADCSRA_BTN, r16
-
-	ldi r16, 0x00
-	sts ADCSRB_BTN, r16
-	ldi r16, 0x40  ;0x40 = 0b01000000
-	sts ADMUX_BTN, r16
-
-	ldi r16, 0;
-	mov BOUNDARY_L, r16
-	ldi r16, high(BUTTON_SELECT_ADC)
-	mov BOUNDARY_H, r16
-	lds	r16, ADCSRA_BTN	
-	ori r16, 0x40
-	sts	ADCSRA_BTN, r16
-	lds DATAL, ADCL_BTN
-	lds DATAH, ADCH_BTN
-	cp DATAL, BOUNDARY_L
-	cpc DATAH, BOUNDARY_H
-	in r16, SREG
-	sbrc r16, 0
-	sts BUTTON_IS_PRESSED, r23
+	;lds	r16, ADCSRA_BTN	
+	;ori r16, 0x40
+	;sts	ADCSRA_BTN, r16
+	;lds DATAL, ADCL_BTN
+	;lds DATAH, ADCH_BTN
+	;cp DATAL, BOUNDARY_L
+	;cpc DATAH, BOUNDARY_H
+	;in r16, SREG
+	;sbrc r16, 0
+	;sts BUTTON_IS_PRESSED, r23
 	;
 	;
 	;
@@ -341,21 +342,6 @@ stop:
 
 
 timer1: ; INTURRUPT HANDLER FOR BUTTONS
-	clr r23
-	ldi r23, 0x01
-	sts BUTTON_IS_PRESSED, r23
-	ldi r16, 0x87  ;0x87 = 0b10000111
-	sts ADCSRA_BTN, r16
-
-	ldi r16, 0x00
-	sts ADCSRB_BTN, r16
-	ldi r16, 0x40  ;0x40 = 0b01000000
-	sts ADMUX_BTN, r16
-
-	ldi r16, 0;
-	mov BOUNDARY_L, r16
-	ldi r16, high(BUTTON_SELECT_ADC)
-	mov BOUNDARY_H, r16
 	lds	r16, ADCSRA_BTN	
 	ori r16, 0x40
 	sts	ADCSRA_BTN, r16
