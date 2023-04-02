21fe04f36244dcc10c10dc496bcfa35df688c6b7
ARFAZ-WINDOWS <arfazhussain@uvic.ca>
2023-04-02 10:19:59 -0700

    Routine Commit: Sun Apr  2 10:19:58 PDT 2023 @arfazhxss

---
+++ 3 Labs/L10/Lab10_p/Lab10_p/main.c
@@ -0,0 +1,18 @@
+/*
+ * Lab10_p.c
+ *
+ * Created: 4/2/2023 3:48:02 AM
+ * Author : arfaz
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
